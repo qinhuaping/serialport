32426bde6e7a148b045b3b08d330262eed8f5721
yuiwong <iam@yuiwong.org>
2017-06-05 19:08:15 +0800

    open close ..

---
+++ yuiwongserialport/include/yuiwong/serialport.hpp
@@ -0,0 +1,221 @@
+/* ========================================================================
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ======================================================================== */
+#ifndef YUIWONGSERIALPORT_SERIALPORT_HPP
+#define YUIWONGSERIALPORT_SERIALPORT_HPP
+#include <stdint.h> /* uint8_t .. */
+#include <termios.h> /* termios */
+#include <sys/types.h> /* ssize_t */
+#include "ev++.h" /* ev_io */
+#include <string> /* string */
+#include "boost/thread.hpp"
+#include "boost/thread/shared_mutex.hpp" /*  shared_mutex */
+namespace yuiwong {
+#if defined(B76800) && (!defined(YUIWONGSERIALPORT_B76800))
+#	define YUIWONGSERIALPORT_B76800 1
+#endif
+#ifndef YUIWONGSERIALPORT_B76800
+#	define YUIWONGSERIALPORT_B76800 0
+#endif
+#if defined(B230400) && (!defined(YUIWONGSERIALPORT_B230400))
+#	define YUIWONGSERIALPORT_B230400 1
+#endif
+#if defined(B4000000) && (!defined(YUIWONGSERIALPORT_B4000000))
+#	define YUIWONGSERIALPORT_B4000000 1
+#endif
+#if (!defined(YUIWONGSERIALPORT_B230400)) && \
+	(!defined(YUIWONGSERIALPORT_B4000000))
+#	define YUIWONGSERIALPORT_B230400 1
+#	define YUIWONGSERIALPORT_B4000000 1
+#endif
+#if defined(_POSIX_VDISABLE) && (!defined(YUIWONGSERIALPORT_POSIXVDISABLE))
+#	define YUIWONGSERIALPORT_POSIXVDISABLE _POSIX_VDISABLE
+#endif
+#ifndef YUIWONGSERIALPORT_POSIXVDISABLE
+#	define YUIWONGSERIALPORT_POSIXVDISABLE '\0'
+#endif
+enum class BaudRateType: uint32_t {
+	_50 = 50,/* posix only */
+	_75 = 75,/* posix only */
+	_134 = 134,/* posix only */
+	_150 = 150,/* posix only */
+	_200 = 200,/* posix only */
+	_1800 = 1800,/* posix only */
+#	if defined(YUIWONGSERIALPORT_B76800) && (YUIWONGSERIALPORT_B76800)
+	_76800 = 76800,/* posix only */
+#	endif
+#	if defined(YUIWONGSERIALPORT_B230400) && \
+	defined(YUIWONGSERIALPORT_B4000000)
+	_230400 = 230400,/* posix only */
+	_460800 = 460800,/* posix only */
+	_500000 = 500000,/* posix only */
+	_576000 = 576000,/* posix only */
+	_921600 = 921600,/* posix only */
+	_1000000 = 1000000,/* posix only */
+	_1152000 = 1152000,/* posix only */
+	_1500000 = 1500000,/* posix only */
+	_2000000 = 2000000,/* posix only */
+	_2500000 = 2500000,/* posix only */
+	_3000000 = 3000000,/* posix only */
+	_3500000 = 3500000,/* posix only */
+	_4000000 = 4000000,/* posix only */
+#	endif
+	_110 = 110,
+	_300 = 300,
+	_600 = 600,
+	_1200 = 1200,
+	_2400 = 2400,
+	_4800 = 4800,
+	_9600 = 9600,
+	_19200 = 19200,
+	_38400 = 38400,
+	_57600 = 57600,
+	_115200 = 115200
+};
+enum class DataBitsType: uint8_t {
+	_5 = 5,
+	_6 = 6,
+	_7 = 7,
+	_8 = 8
+};
+enum class ParityType: int {
+	NONE,
+	ODD,
+	EVEN,
+	SPACE
+};
+enum class StopBitsType: int {
+	_1,
+	_2
+};
+enum class FlowType: int {
+	OFF,
+	HARDWARE,
+	XONXOFF
+};
+/**
+ * @name PortSetting
+ * structure to contain port settings
+ */
+struct PortSetting {
+	BaudRateType baudRate;
+	DataBitsType dataBits;
+	ParityType parityType;
+	StopBitsType stopBitsType;
+	FlowType flowType;
+	ssize_t timeoutMillisec;
+};
+class SerialPort {
+public:
+	enum class QueryMode {
+		Polling,
+		EventDriven
+	};
+	enum class Dirty: uint32_t {
+		BaudRate = 0x0001,
+		Parity = 0x0002,
+		StopBits = 0x0004,
+		DataBits = 0x0008,
+		Flow = 0x0010,
+		TimeOut = 0x0100,
+		ALL = 0x0fff,
+		SettingMask = 0x00ff,/* without TimeOut */
+	};
+	enum class OpenFlag: uint32_t {
+		NotOpen = 0x0000,
+		Read = 0x0004,
+		Write = 0x0002,
+		ReadWrite = Read | Write,
+		Append = 0x0008,
+		Truncate = 0x0010,
+		Text = 0x0020,
+	};
+	SerialPort(QueryMode const& queryMode = QueryMode::EventDriven);
+	SerialPort(
+		std::string const& name,
+		QueryMode const& mode = QueryMode::EventDriven);
+	SerialPort(
+		PortSetting const& portSetting,
+		QueryMode const& mode = QueryMode::EventDriven);
+	SerialPort(
+		std::string const& name,
+		PortSetting const& portSetting,
+		QueryMode const& mode = QueryMode::EventDriven);
+	~SerialPort();
+	inline QueryMode const& getQueryMode() const { return this->queryMode; }
+	std::string getPortName() const;
+	void setPortName(std::string const& portName);
+	bool isOpen() const;
+	/**
+	 * @brief
+	 * opens a serial port and sets its OpenFlag to @a openFlag
+	 * (if not NotOpen)
+	 * @note that this function does not specify which device to open
+	 * @return
+	 * - return 0 if no any error
+	 * - return <0 (-errno) when fail
+	 * - return >0 (errno) when alreay done
+	 * @details
+	 * - this function has no effect if the port associated with the class is
+	 *   already open and return is EBUSY
+	 * - the port is also configured to the current settings,
+	 *   as stored in the settings structure
+	 * - if do open start will be called
+	 */
+	int open(
+		uint32_t const openFlag = static_cast<uint32_t>(OpenFlag::ReadWrite),
+		bool const asyncSend = false);
+	/** @brief stop (if started) and close */
+	int close();
+	int start(bool const asyncSend = false);
+	int stop();
+	/** @interface shouldRecv should read data from fd */
+	virtual ssize_t shouldRecv(int const fd) = 0;
+	/** @brief override to send to fd when just fd writeable */
+	virtual ssize_t shouldSend(int const fd);
+	void updatePortSettings();
+	static std::string getFullPortName(std::string const& portName);
+	static struct termios& baudRateToTermios(
+		uint32_t const& baudRate,
+		struct termios& ter);
+private:
+	static void handlerw(struct ev_loop* mainLoop, ev_io* rwio, int event);
+	void rwioloop();
+	inline bool __isOpen() const { return this->device.fd >= 0; }
+	int __open(
+		uint32_t const openFlag = static_cast<uint32_t>(OpenFlag::ReadWrite),
+		bool const asyncSend = false);
+	int __start(bool const asyncSend = false);
+	void __updatePortSettings();
+	mutable boost::shared_mutex rwlock;
+	QueryMode const queryMode;
+	PortSetting portSetting;
+	uint32_t portSettingDirty;
+	struct {
+		int fd;/* serial device fd */
+		std::string portName;
+		uint32_t openFlag;
+		struct termios oldTermios;
+		struct termios currentTermios;
+	} device;
+	struct {
+		ev_io rwio;
+		struct ev_loop* mainLoop;
+		bool running;
+		bool terminate;
+		boost::shared_ptr<boost::thread> thread;
+	} eventDrive;
+};
+}
+#endif
---
+++ yuiwongserialport/serialport.cpp
@@ -0,0 +1,598 @@
+/* ========================================================================
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ======================================================================== */
+#include "yuiwong/serialport.hpp" /* this header */
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "boost/make_shared.hpp"
+#define NAME "yuiwongserialport"
+namespace yuiwong
+{
+SerialPort::SerialPort(QueryMode const& queryMode): queryMode(queryMode)
+{
+	this->portSetting.baudRate = BaudRateType::_9600;
+	this->portSetting.parityType = ParityType::NONE;
+	this->portSetting.flowType = FlowType::OFF;
+	this->portSetting.dataBits = DataBitsType::_8;
+	this->portSetting.stopBitsType =  StopBitsType::_1;
+	this->portSetting.timeoutMillisec = 10;
+	this->portSettingDirty = static_cast<uint32_t>(Dirty::ALL);
+	this->device.fd = -1;
+	this->device.portName.assign("/dev/ttyUSB0");
+	this->eventDrive.mainLoop = nullptr;
+	this->eventDrive.running = false;
+}
+SerialPort::~SerialPort()
+{
+	this->close();
+}
+std::string SerialPort::getPortName() const
+{
+	boost::shared_lock<boost::shared_mutex> rLock(this->rwlock);
+	(void)(rLock);
+	return this->device.portName;
+}
+/**
+ * set @a portName of the device associated with the object, e.g. "/dev/ttyS0"
+ */
+void SerialPort::setPortName(std::string const& portName)
+{
+	boost::upgrade_lock<boost::shared_mutex> uplock(this->rwlock);
+	boost::upgrade_to_unique_lock<boost::shared_mutex> wLock(uplock);
+	(void)(wLock);
+	this->device.portName = portName;
+}
+void SerialPort::handlerw(
+	struct ev_loop* /* mainLoop */,
+	ev_io* rwio,
+	int event)
+{
+	if (!rwio) {
+		std::cerr << "[" NAME "][ERRO](" << __FILE__ << "+" << __LINE__ << ") "
+			"no rwio\n";
+		return;
+	}
+	SerialPort *const self = reinterpret_cast<SerialPort *>(rwio->data);
+	if (!self) {
+		std::cerr << "[" NAME "][ERRO](" << __FILE__ << "+" << __LINE__ << ") "
+			"no instance\n";
+		return;
+	}
+	{
+		boost::shared_lock<boost::shared_mutex> rLock(self->rwlock);
+		(void)(rLock);
+		if (self->eventDrive.terminate) {
+			std::cerr << "[" NAME "][WARN](" << __FILE__ << "+" << __LINE__ <<
+				") terminate called\n";
+			return;
+		}
+	}
+	if (event & EV_READ) {
+		ssize_t const ret = self->shouldRecv(self->device.fd);
+		if (ret < 0) {
+			std::cerr << "[" NAME "][ERRO](" << __FILE__ << "+" << __LINE__ <<
+				") recv fail: break\n";
+			return;
+		}
+	}
+	{
+		boost::shared_lock<boost::shared_mutex> rLock(self->rwlock);
+		(void)(rLock);
+		if (self->eventDrive.terminate) {
+			std::cerr << "[" NAME "][WARN](" << __FILE__ << "+" << __LINE__ <<
+				") terminate called\n";
+			return;
+		}
+	}
+	if (event & EV_WRITE) {
+		ssize_t const ret = self->shouldSend(self->device.fd);
+		if (ret < 0) {
+			std::cerr << "[" NAME "][ERRO](" << __FILE__ << "+" << __LINE__ <<
+				") send fail!\n";
+		}
+	}
+}
+void SerialPort::rwioloop()
+{
+	{
+		boost::upgrade_lock<boost::shared_mutex> uplock(this->rwlock);
+		boost::upgrade_to_unique_lock<boost::shared_mutex> wLock(uplock);
+		(void)(wLock);
+		this->eventDrive.running = true;
+	}
+	ev_run(this->eventDrive.mainLoop, 0);
+	{
+		boost::upgrade_lock<boost::shared_mutex> uplock(this->rwlock);
+		boost::upgrade_to_unique_lock<boost::shared_mutex> wLock(uplock);
+		(void)(wLock);
+		this->eventDrive.running = false;
+	}
+}
+bool SerialPort::isOpen() const
+{
+	boost::shared_lock<boost::shared_mutex> rLock(this->rwlock);
+	(void)(rLock);
+	return (this->device.fd >= 0);
+}
+/**
+ * @brief
+ * opens a serial port and sets its OpenFlag to @a openFlag (if not NotOpen)
+ * @note that this function does not specify which device to open
+ * @return
+ * - return 0 if no any error
+ * - return <0 (-errno) when fail
+ * - return >0 (errno) when alreay done
+ * @details
+ * - this function has no effect if the port associated with the class is
+ *   already open and return is EBUSY
+ * - the port is also configured to the current settings,
+ *   as stored in the settings structure
+ * - if do open start will be called
+ */
+int SerialPort::open(uint32_t const openFlag, bool const asyncSend)
+{
+	boost::upgrade_lock<boost::shared_mutex> uplock(this->rwlock);
+	boost::upgrade_to_unique_lock<boost::shared_mutex> wLock(uplock);
+	(void)(wLock);
+	if (this->__isOpen()) {
+		return EBUSY;
+	}
+	if (openFlag == static_cast<uint32_t>(OpenFlag::NotOpen)) {
+		return -EINVAL;
+	}
+	return this->__open(openFlag, asyncSend);
+}
+int SerialPort::__open(uint32_t const openFlag, bool const asyncSend)
+{
+	/** @note linux 2.6.21 seems to ignore O_NDELAY flag */
+	int copenFlag = O_NOCTTY | O_NDELAY;
+	if (openFlag & static_cast<uint32_t>(OpenFlag::ReadWrite)) {
+		copenFlag |= O_RDWR;
+	} else if (openFlag & static_cast<uint32_t>(OpenFlag::Read)) {
+		copenFlag |= O_RDONLY;
+	} else if (openFlag & static_cast<uint32_t>(OpenFlag::Write)) {
+		copenFlag |= O_WRONLY;
+	}
+	if ((openFlag & static_cast<uint32_t>(OpenFlag::Write)) &&
+		(openFlag & static_cast<uint32_t>(OpenFlag::Append))) {
+		copenFlag |= O_APPEND;
+	}
+	int const fd = ::open(
+		getFullPortName(this->device.portName).c_str(),
+		/* O_RDWR | O_NOCTTY | O_NDELAY */ copenFlag);
+	if (-1 == fd) {
+		int const e = errno;
+		std::cerr << "[" NAME "][WARN](" << __FILE__ << "+" << __LINE__ << ") "
+			"open `" << this->device.portName << "' fail " << strerror(e) <<
+			"\n";
+		if (e) {
+			return -e;
+		} else {
+			return -EPERM;
+		}
+	}
+	this->device.fd = fd;
+	this->device.openFlag = openFlag;/* flag the port as opened */
+	::tcgetattr(fd, &this->device.oldTermios);/* save the old termios */
+	/* make a working copy */
+	this->device.currentTermios = this->device.oldTermios;
+	::cfmakeraw(&this->device.currentTermios);/* enable raw access */
+	/* set up other port settings */
+	this->device.currentTermios.c_cflag |= CREAD|CLOCAL;
+	this->device.currentTermios.c_lflag &=
+		(~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ISIG));
+	this->device.currentTermios.c_iflag &=
+		(~(INPCK | IGNPAR | PARMRK | ISTRIP | ICRNL | IXANY));
+	this->device.currentTermios.c_oflag &= (~OPOST);
+	this->device.currentTermios.c_cc[VMIN] = 0;
+	/* is a disable character available on this system? */
+#	ifdef YUIWONGSERIALPORT_POSIXVDISABLE
+	/*
+	 * some systems allow for per-device disable-characters,
+	 * so get the proper value for the configured device
+	 */
+	long const vdisable = ::fpathconf(fd, _PC_VDISABLE);
+	this->device.currentTermios.c_cc[VINTR] = vdisable;
+	this->device.currentTermios.c_cc[VQUIT] = vdisable;
+	this->device.currentTermios.c_cc[VSTART] = vdisable;
+	this->device.currentTermios.c_cc[VSTOP] = vdisable;
+	this->device.currentTermios.c_cc[VSUSP] = vdisable;
+#	endif /* YUIWONGSERIALPORT_POSIXVDISABLE */
+	this->portSettingDirty = static_cast<uint32_t>(Dirty::ALL);
+	this->__updatePortSettings();
+	return this->__start(asyncSend);
+}
+/** @brief stop (if started) and close */
+int SerialPort::close()
+{
+	this->stop();
+	{
+		boost::upgrade_lock<boost::shared_mutex> uplock(this->rwlock);
+		boost::upgrade_to_unique_lock<boost::shared_mutex> wLock(uplock);
+		(void)(wLock);
+		if (this->device.fd > 0) {
+			::fsync(this->device.fd);
+			int const ret = ::close(this->device.fd);
+			this->device.fd = -1;
+			return ret;
+		} else {
+			return ENOENT;
+		}
+	}
+}
+int SerialPort::start(bool const asyncSend)
+{
+	boost::upgrade_lock<boost::shared_mutex> uplock(this->rwlock);
+	boost::upgrade_to_unique_lock<boost::shared_mutex> wLock(uplock);
+	(void)(wLock);
+	if (this->device.fd < 0) {
+		std::cerr << "[" NAME "][WARN](" << __FILE__ << "+" << __LINE__ << ") "
+			<< "device not open open it now with rw\n";
+		return this->__open(static_cast<uint32_t>(OpenFlag::ReadWrite));
+	}
+	return this->__start(asyncSend);
+}
+int SerialPort::__start(bool const asyncSend)
+{
+	if (QueryMode::EventDriven == this->queryMode) {
+		if (this->eventDrive.mainLoop) {
+			return EBUSY;
+		}
+		this->eventDrive.mainLoop = ev_default_loop(0);
+		/* io 监控器的初始化 */
+		ev_init(&this->eventDrive.rwio, handlerw);
+		this->eventDrive.rwio.data = this;
+		if (asyncSend && (this->device.openFlag &
+			(static_cast<uint32_t>(OpenFlag::Write)))) {
+			ev_io_set(
+				&this->eventDrive.rwio,
+				this->device.fd,
+				EV_READ | EV_WRITE);
+		} else {
+			ev_io_set(
+				&this->eventDrive.rwio,
+				this->device.fd,
+				EV_READ);
+		}
+		this->eventDrive.terminate = false;
+		ev_io_start(this->eventDrive.mainLoop, &this->eventDrive.rwio);
+		this->eventDrive.thread = boost::make_shared<boost::thread>(
+			boost::bind(&SerialPort::rwioloop, this));
+	}
+	return 0;
+}
+int SerialPort::stop()
+{
+	{
+		boost::shared_lock<boost::shared_mutex> rLock(this->rwlock);
+		(void)(rLock);
+		if (QueryMode::EventDriven != this->queryMode) {
+			return 0;
+		}
+	}
+	struct ev_loop* mainLoop;
+	{
+		boost::upgrade_lock<boost::shared_mutex> uplock(this->rwlock);
+		boost::upgrade_to_unique_lock<boost::shared_mutex> wLock(uplock);
+		(void)(wLock);
+		if (!this->eventDrive.mainLoop) {
+			return ENOENT;
+		}
+		this->eventDrive.terminate = true;
+		mainLoop = this->eventDrive.mainLoop;
+		this->eventDrive.mainLoop = nullptr;
+		/*
+		 * for one-shot events, one must manually stop the watcher
+         * with its corresponding stop function
+		 */
+		ev_io_stop(mainLoop, &this->eventDrive.rwio);
+		/* this causes all nested ev_run's to stop iterating */
+		ev_break(mainLoop, EVBREAK_ALL);
+		/*
+		ev_clear_pending(mainLoop, &this->eventDrive.rwio);
+		*/
+	}
+	{
+		bool r;
+		{
+			r = this->eventDrive.running;
+			boost::shared_lock<boost::shared_mutex> rLock(this->rwlock);
+			(void)(rLock);
+		}
+		while (r) {
+			usleep(10 * 1e3);
+			{
+				boost::shared_lock<boost::shared_mutex> rLock(this->rwlock);
+				(void)(rLock);
+				r = this->eventDrive.running;
+			}
+		}
+		this->eventDrive.thread->join();
+		{
+			boost::upgrade_lock<boost::shared_mutex> uplock(this->rwlock);
+			boost::upgrade_to_unique_lock<boost::shared_mutex> wLock(uplock);
+			(void)(wLock);
+			this->eventDrive.thread.reset();
+		}
+		ev_loop_destroy(/* EV_DEFAULT */ mainLoop);/* FIXME: release */
+		mainLoop = nullptr;
+		return 0;
+	}
+}
+/** @brief override to send to fd when just fd writeable */
+ssize_t SerialPort::shouldSend(int const fd)
+{
+	std::cout << "[" NAME "][INFO](" << __FILE__ << "+" << __LINE__ << ") "
+		<< "defualt implements nothing to send to fd `" << fd << "' flush\n";
+	fsync(fd);
+	return 0;
+}
+void SerialPort::updatePortSettings()
+{
+	boost::upgrade_lock<boost::shared_mutex> uplock(this->rwlock);
+	boost::upgrade_to_unique_lock<boost::shared_mutex> wLock(uplock);
+	(void)(wLock);
+	if ((!this->__isOpen()) || (!this->portSettingDirty)) {
+		return;
+	} else {
+		return this->__updatePortSettings();
+	}
+}
+void SerialPort::__updatePortSettings()
+{
+	if (this->portSettingDirty & static_cast<uint32_t>(Dirty::BaudRate)) {
+		switch (this->portSetting.baudRate) {
+		case BaudRateType::_50:
+			baudRateToTermios(B50, this->device.currentTermios);
+			break;
+		case BaudRateType::_75:
+			baudRateToTermios(B75, this->device.currentTermios);
+			break;
+		case BaudRateType::_110:
+			baudRateToTermios(B110, this->device.currentTermios);
+			break;
+		case BaudRateType::_134:
+			baudRateToTermios(B134, this->device.currentTermios);
+			break;
+		case BaudRateType::_150:
+			baudRateToTermios(B150, this->device.currentTermios);
+			break;
+		case BaudRateType::_200:
+			baudRateToTermios(B200, this->device.currentTermios);
+			break;
+		case BaudRateType::_300:
+			baudRateToTermios(B300, this->device.currentTermios);
+			break;
+		case BaudRateType::_600:
+			baudRateToTermios(B600, this->device.currentTermios);
+			break;
+		case BaudRateType::_1200:
+			baudRateToTermios(B1200, this->device.currentTermios);
+			break;
+		case BaudRateType::_1800:
+			baudRateToTermios(B1800, this->device.currentTermios);
+			break;
+		case BaudRateType::_2400:
+			baudRateToTermios(B2400, this->device.currentTermios);
+			break;
+		case BaudRateType::_4800:
+			baudRateToTermios(B4800, this->device.currentTermios);
+			break;
+		case BaudRateType::_9600:
+			baudRateToTermios(B9600, this->device.currentTermios);
+			break;
+		case BaudRateType::_19200:
+			baudRateToTermios(B19200, this->device.currentTermios);
+			break;
+		case BaudRateType::_38400:
+			baudRateToTermios(B38400, this->device.currentTermios);
+			break;
+		case BaudRateType::_57600:
+			baudRateToTermios(B57600, this->device.currentTermios);
+			break;
+#		if defined(YUIWONGSERIALPORT_B76800) && (YUIWONGSERIALPORT_B76800)
+		case BaudRateType::_76800:
+			baudRateToTermios(B76800, this->device.currentTermios);
+			break;
+#		endif
+		case BaudRateType::_115200:
+			baudRateToTermios(B115200, this->device.currentTermios);
+			break;
+#		if defined(YUIWONGSERIALPORT_B230400) && \
+			defined(YUIWONGSERIALPORT_B4000000)
+		case BaudRateType::_230400:
+			baudRateToTermios(B230400, this->device.currentTermios);
+			break;
+		case BaudRateType::_460800:
+			baudRateToTermios(B460800, this->device.currentTermios);
+			break;
+		case BaudRateType::_500000:
+			baudRateToTermios(B500000, this->device.currentTermios);
+			break;
+		case BaudRateType::_576000:
+			baudRateToTermios(B576000, this->device.currentTermios);
+			break;
+		case BaudRateType::_921600:
+			baudRateToTermios(B921600, this->device.currentTermios);
+			break;
+		case BaudRateType::_1000000:
+			baudRateToTermios(B1000000, this->device.currentTermios);
+			break;
+		case BaudRateType::_1152000:
+			baudRateToTermios(B1152000, this->device.currentTermios);
+			break;
+		case BaudRateType::_1500000:
+			baudRateToTermios(B1500000, this->device.currentTermios);
+			break;
+		case BaudRateType::_2000000:
+			baudRateToTermios(B2000000, this->device.currentTermios);
+			break;
+		case BaudRateType::_2500000:
+			baudRateToTermios(B2500000, this->device.currentTermios);
+			break;
+		case BaudRateType::_3000000:
+			baudRateToTermios(B3000000, this->device.currentTermios);
+			break;
+		case BaudRateType::_3500000:
+			baudRateToTermios(B3500000, this->device.currentTermios);
+			break;
+		case BaudRateType::_4000000:
+			baudRateToTermios(B4000000, this->device.currentTermios);
+			break;
+#		endif
+		default:
+			std::cerr << "[" NAME "][WARN](" << __FILE__ << "+" << __LINE__
+				<< ") bad baudRate no update " <<
+				static_cast<uint32_t>(this->portSetting.baudRate) << "\n";
+			break;
+		}
+	}
+	if (this->portSettingDirty & static_cast<uint32_t>(Dirty::Parity)) {
+		switch (this->portSetting.parityType) {
+		case ParityType::SPACE:
+			/*
+			 * space parity not directly supported - add an extra data bit to
+			 * simulate it
+			 */
+			this->portSettingDirty |= static_cast<uint32_t>(Dirty::DataBits);
+			break;
+		case ParityType::NONE:
+			this->device.currentTermios.c_cflag &= (~PARENB);
+			break;
+		case ParityType::EVEN: {
+			this->device.currentTermios.c_cflag &= (~PARODD);
+			this->device.currentTermios.c_cflag |= PARENB;
+		} break;
+		case ParityType::ODD:
+			this->device.currentTermios.c_cflag |= (PARENB|PARODD);
+			break;
+		}
+	}
+	/* must after parity settings */
+	if (this->portSettingDirty & static_cast<uint32_t>(Dirty::DataBits)) {
+		if (this->portSetting.parityType != ParityType::SPACE) {
+			this->device.currentTermios.c_cflag &= (~CSIZE);
+			switch(this->portSetting.dataBits) {
+			case DataBitsType::_5:
+				this->device.currentTermios.c_cflag |= CS5;
+				break;
+			case DataBitsType::_6:
+				this->device.currentTermios.c_cflag |= CS6;
+				break;
+			case DataBitsType::_7:
+				this->device.currentTermios.c_cflag |= CS7;
+				break;
+			case DataBitsType::_8:
+				this->device.currentTermios.c_cflag |= CS8;
+				break;
+			}
+		} else {
+			/*
+			 * space parity not directly supported - add an extra data bit to
+			 * simulate it
+			 */
+			this->device.currentTermios.c_cflag &= ~(PARENB|CSIZE);
+			switch(this->portSetting.dataBits) {
+			case DataBitsType::_5:
+				this->device.currentTermios.c_cflag |= CS6;
+				break;
+			case DataBitsType::_6:
+				this->device.currentTermios.c_cflag |= CS7;
+				break;
+			case DataBitsType::_7:
+				this->device.currentTermios.c_cflag |= CS8;
+				break;
+			case DataBitsType::_8:
+				/* this will never happen, put here to suppress an warning */
+				std::cerr << "[" NAME "][WARN](" << __FILE__ << "+" << __LINE__
+					<< ") DataBits _8 no update\n";
+				break;
+			}
+		}
+	}
+	if (this->portSettingDirty & static_cast<uint32_t>(Dirty::StopBits)) {
+		switch (this->portSetting.stopBitsType) {
+		case StopBitsType::_1:
+			this->device.currentTermios.c_cflag &= (~CSTOPB);
+			break;
+		case StopBitsType::_2:
+			this->device.currentTermios.c_cflag |= CSTOPB;
+			break;
+		}
+	}
+	if (this->portSettingDirty & static_cast<uint32_t>(Dirty::Flow)) {
+		switch(this->portSetting.flowType) {
+		case FlowType::OFF:
+			this->device.currentTermios.c_cflag &= (~CRTSCTS);
+			this->device.currentTermios.c_iflag &= (~(IXON|IXOFF|IXANY));
+			break;
+		case FlowType::XONXOFF:
+			/* software (XON/XOFF) flow control */
+			this->device.currentTermios.c_cflag &= (~CRTSCTS);
+			this->device.currentTermios.c_iflag |= (IXON|IXOFF|IXANY);
+			break;
+		case FlowType::HARDWARE:
+			this->device.currentTermios.c_cflag |= CRTSCTS;
+			this->device.currentTermios.c_iflag &= (~(IXON|IXOFF|IXANY));
+			break;
+		}
+	}
+
+	/* if any thing in currentTermios changed, flush */
+	if (this->portSettingDirty & static_cast<uint32_t>(Dirty::SettingMask)) {
+		::tcsetattr(this->device.fd, TCSAFLUSH, &this->device.currentTermios);
+	}
+	if (this->portSettingDirty & static_cast<uint32_t>(Dirty::TimeOut)) {
+		long const millisec = this->portSetting.timeoutMillisec;
+		if (millisec == -1) {
+			::fcntl(this->device.fd, F_SETFL, O_NDELAY);
+		} else {
+			/*
+			 * O_SYNC should enable blocking ::write()
+			 * however this seems not working on Linux 2.6.21
+			 * (works on OpenBSD 4.2)
+			 */
+			::fcntl(this->device.fd, F_SETFL, O_SYNC);
+		}
+		::tcgetattr(this->device.fd, &this->device.currentTermios);
+		this->device.currentTermios.c_cc[VTIME] = millisec / 100;
+		::tcsetattr(this->device.fd, TCSAFLUSH, &this->device.currentTermios);
+	}
+	this->portSettingDirty = 0;
+}
+std::string SerialPort::getFullPortName(std::string const& portName)
+{
+	char const* const p = strstr(portName.c_str(), "/");
+	if (p && (p == portName.c_str())) {
+		return portName;
+	} else {
+		return "/dev/" + portName;
+	}
+}
+struct termios& SerialPort::baudRateToTermios(
+	uint32_t const& baudRate,
+	struct termios& ter)
+{
+#	ifdef CBAUD
+	ter.c_cflag &= (~CBAUD);
+	ter.c_cflag |= baudRate;
+#	else
+	::cfsetispeed(&ter, baudRate);
+	::cfsetospeed(&ter, baudRate);
+#	endif
+	return ter;
+}
+}
